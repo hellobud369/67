d4bf2ae9133293eae75bb99dec7a7190fc539395
hellobud369 <1099059@student.ccgs.wa.edu.au>
2025-09-09 08:41:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+let currentSound = null;
+
 const buttons = document.querySelectorAll('.soundboard button');
+const stopButton = document.getElementById('stop-sound');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const sound = new Audio(button.dataset.sound);
-    sound.play();
+    // Stop current sound if one is playing
+    if (currentSound) {
+      currentSound.pause();
+      currentSound.currentTime = 0;
+    }
+    // Play new sound
+    currentSound = new Audio(button.dataset.sound);
+    currentSound.play();
   });
+});
+
+stopButton.addEventListener('click', () => {
+  if (currentSound) {
+    currentSound.pause();
+    currentSound.currentTime = 0;
+    currentSound = null;
+  }
 });
